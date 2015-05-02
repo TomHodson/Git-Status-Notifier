6444191a43baf223e4ce23fa7aff1025dc3ba5cc
Tom <thomas.c.hodson@gmail.com>
2015-05-02 12:35:36 +0100

    create command line flag to enable emailing, just prints otherwise

--- github_checker.py
+++ github_checker.py
@@ -2,6 +2,14 @@ from os import listdir, path
 from subprocess import check_output
 from time import gmtime, strftime
 
+dryrun = True
+import sys
+try:
+	dryrun = False if sys.argv[1] == "email" else True
+except:
+	pass
+
+
 github_folder = "/Users/tom/Code/github"
 output_format = \
 """
@@ -17,17 +25,19 @@ number_of_orphan_files = 0
 for f in listdir(github_folder):
 	full_path = path.join(github_folder, f)
 	if path.isdir(full_path):
-		status = check_output(["git", "status"], cwd = full_path)
+		full_status = check_output(["git", "status"], cwd = full_path)
+		short_status = check_output(["git", "status", "--porcelain"], cwd = full_path)
+
 		time = strftime("%a, %d %b", gmtime(path.getmtime(full_path)))
-		if "up-to-date" not in status:
+		if short_status:
 			pressing_repos.append(output_format.format(repo_name = f,
 			last_modified = time,
-			status = status, 
+			status = full_status, 
 			underline = '#' * (len(time) + len(f) + 2)))
 	else: number_of_orphan_files += 1
 
 if number_of_orphan_files:
-	output.append("You have {n} orphaned files in your git directory that need homes.".format(n = number_of_orphan_files))
+	pressing_repos.append("You have {n} orphaned files in your git directory that need homes.".format(n = number_of_orphan_files))
 
 output =  "".join(pressing_repos)
 
@@ -38,7 +48,7 @@ if output:
 	#Next, log in to the server
 	server.ehlo()
 	server.starttls()
-	server.login("thomas.c.hodson@gmail.com", open("./secret").read())
+	server.login("thomas.c.hodson@gmail.com", open("secret").read())
 	msg = "\r\n".join([
 	  "From: thomas.c.hodson@gmail.com",
 	  "To: thomas.c.hodson@gmail.com",
@@ -47,5 +57,7 @@ if output:
 	  output
 	  ])
 
-	#Send the mail
-	server.sendmail("thomas.c.hodson@gmail.com", "thomas.c.hodson@gmail.com", msg)
+	if not dryrun:
+		server.sendmail("thomas.c.hodson@gmail.com", "thomas.c.hodson@gmail.com", msg)
+	else:
+		print msg
